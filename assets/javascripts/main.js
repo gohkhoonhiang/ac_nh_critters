9efa37f7c356e0f9ca1ea7cdf88a126ea3dd308f
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2020-07-01 20:30:57 +0800

    Add filter to show only critters not yet donated

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -81,11 +81,13 @@ var app = new Vue({
     fish_lookup_time: null,
     toggle_fish_hemisphere: ['N','S'],
     toggle_fish_new_this_month: false,
+    toggle_fish_donated: false,
 
     bug_lookup_time_input: null,
     bug_lookup_time: null,
     toggle_bug_hemisphere: ['N','S'],
     toggle_bug_new_this_month: false,
+    toggle_bug_donated: false,
 
     fish_high_price_threshold: 1000,
     bug_high_price_threshold: 1000,
@@ -198,13 +200,16 @@ var app = new Vue({
       });
     },
 
-    filterCurrentHour: function(data, lookup_time, selected_hemispheres) {
+    filterCurrentHour: function(data, lookup_time, selected_hemispheres, toggle_donated) {
       var vm = this;
       var filter_time = lookup_time ? lookup_time : vm.now;
       var current_month = filter_time.getMonth() + 1;
       var current_hour = filter_time.getHours();
       return data.filter(function(row) {
-        return row.months.includes(current_month) && row.hours.includes(current_hour) && selected_hemispheres.includes(row.hemisphere);
+        return row.months.includes(current_month) &&
+          row.hours.includes(current_hour) &&
+          selected_hemispheres.includes(row.hemisphere) &&
+          (!toggle_donated || !row.donated);
       });
     },
 
@@ -212,43 +217,60 @@ var app = new Vue({
       var vm = this;
       var this_month = vm.now.getMonth() + 1;
       var next_month = this_month + 1;
-      return data.filter(row => row.months.includes(this_month) && !row.months.includes(next_month) && selected_hemispheres.includes(row.hemisphere));
+      return data.filter(function(row) {
+        return row.months.includes(this_month) &&
+          !row.months.includes(next_month) &&
+          selected_hemispheres.includes(row.hemisphere);
+      })
     },
 
     filterIncoming: function(data, selected_hemispheres) {
       var vm = this;
       var this_month = vm.now.getMonth() + 1;
       var next_month = this_month + 1;
-      return data.filter(row => !row.months.includes(this_month) && row.months.includes(next_month) && selected_hemispheres.includes(row.hemisphere));
+      return data.filter(function(row) {
+        return !row.months.includes(this_month) &&
+          row.months.includes(next_month) &&
+          selected_hemispheres.includes(row.hemisphere);
+      });
     },
 
-    filterThisMonth: function(data, selected_hemispheres, new_this_month) {
+    filterThisMonth: function(data, selected_hemispheres, new_this_month, toggle_donated) {
       var vm = this;
       var this_month = vm.now.getMonth() + 1;
-      return data.filter(row => row.months.includes(this_month) && selected_hemispheres.includes(row.hemisphere) && (!new_this_month || vm.newThisMonth(row)));
+      return data.filter(function(row) {
+        return row.months.includes(this_month) &&
+          selected_hemispheres.includes(row.hemisphere) &&
+          (!new_this_month || vm.newThisMonth(row)) &&
+          (!toggle_donated || !row.donated)
+      });
     },
 
-    filterComplete: function(data, selected_hemispheres, month_filter) {
+    filterComplete: function(data, selected_hemispheres, month_filter, toggle_donated) {
       var vm = this;
-      return data.filter(row => selected_hemispheres.includes(row.hemisphere) && (!month_filter || month_filter.length === 0 || row.month_names.some(m => month_filter.includes(m))));
+      return data.filter(function(row) {
+        return selected_hemispheres.includes(row.hemisphere) &&
+          (!month_filter || month_filter.length === 0 || row.month_names.some(m => month_filter.includes(m))) &&
+          (!toggle_donated || !row.donated)
+      });
     },
 
     filterFishData: function() {
       var vm = this;
-      vm.current_hour_fish_data = vm.filterCurrentHour(vm.fish_data, vm.fish_lookup_time, vm.toggle_fish_hemisphere);
+      vm.current_hour_fish_data = vm.filterCurrentHour(vm.fish_data, vm.fish_lookup_time, vm.toggle_fish_hemisphere, vm.toggle_fish_donated);
       vm.outgoing_fish_data = vm.filterOutgoing(vm.fish_data, vm.toggle_fish_hemisphere);
       vm.incoming_fish_data = vm.filterIncoming(vm.fish_data, vm.toggle_fish_hemisphere);
-      vm.this_month_fish_data = vm.filterThisMonth(vm.fish_data, vm.toggle_fish_hemisphere, vm.toggle_fish_new_this_month);
-      vm.complete_fish_data = vm.filterComplete(vm.fish_data, vm.toggle_fish_hemisphere, vm.fish_month_filter);
+      vm.this_month_fish_data = vm.filterThisMonth(vm.fish_data, vm.toggle_fish_hemisphere, vm.toggle_fish_new_this_month, vm.toggle_fish_donated);
+      vm.complete_fish_data = vm.filterComplete(vm.fish_data, vm.toggle_fish_hemisphere, vm.fish_month_filter, vm.toggle_fish_donated);
     },
 
     filterBugData: function() {
       var vm = this;
-      vm.current_hour_bug_data = vm.filterCurrentHour(vm.bug_data, vm.bug_lookup_time, vm.toggle_bug_hemisphere);
+      vm.current_hour_bug_data = vm.filterCurrentHour(vm.bug_data, vm.bug_lookup_time, vm.toggle_bug_hemisphere, vm.toggle_bug_donated);
       vm.outgoing_bug_data = vm.filterOutgoing(vm.bug_data, vm.toggle_bug_hemisphere);
       vm.incoming_bug_data = vm.filterIncoming(vm.bug_data, vm.toggle_bug_hemisphere);
-      vm.this_month_bug_data = vm.filterThisMonth(vm.bug_data, vm.toggle_bug_hemisphere, vm.toggle_bug_new_this_month);
-      vm.complete_bug_data = vm.filterComplete(vm.bug_data, vm.toggle_bug_hemisphere, vm.bug_month_filter);
+      vm.this_month_bug_data = vm.filterThisMonth(vm.bug_data, vm.toggle_bug_hemisphere, vm.toggle_bug_new_this_month, vm.toggle_bug_donated);
+      vm.complete_bug_data = vm.filterComplete(vm.bug_data, vm.toggle_bug_hemisphere, vm.bug_month_filter, vm.toggle_bug_donated);
     },
 
     newThisMonth: function(row) {
@@ -330,7 +352,9 @@ var app = new Vue({
       var vm = this;
       var settings = {
         toggle_fish_hemisphere: vm.toggle_fish_hemisphere,
+        toggle_fish_donated: vm.toggle_fish_donated,
         toggle_bug_hemisphere: vm.toggle_bug_hemisphere,
+        toggle_bug_donated: vm.toggle_bug_donated,
         fish_high_price_threshold: vm.fish_high_price_threshold,
         bug_high_price_threshold: vm.bug_high_price_threshold,
         fish_month_filter: vm.fish_month_filter,
@@ -401,6 +425,12 @@ var app = new Vue({
       vm.storeSettings();
     },
 
+    toggle_fish_donated: function(new_val, old_val) {
+      var vm = this;
+      vm.filterFishData();
+      vm.storeSettings();
+    },
+
     toggle_bug_hemisphere: function(new_val, old_val) {
       var vm = this;
       vm.filterBugData();
@@ -408,6 +438,12 @@ var app = new Vue({
     },
 
     toggle_bug_new_this_month: function(new_val, old_val) {
+      var vm = this;
+      vm.filterBugData();
+      vm.storeSettings();
+    },
+
+    toggle_bug_donated: function(new_val, old_val) {
       var vm = this;
       vm.filterBugData();
       vm.storeSettings();
